916cfb63ae53305a6434c278247ccd9ad7724e13
lPatrickll <patrickgonzalesquintana@gmail.com>
2024-05-07 10:13:05 -0400

    Se mejoro la funcion buscarProyectos para que devuelva el nombre de un proyecto

--- src/buscar.spec.js
+++ src/buscar.spec.js
@@ -3,8 +3,16 @@ describe("Buscar", () => {
     let proyectos = [];
     expect(buscarProyectos("proyecto1", proyectos)).toEqual("");
   });
+
+  it("no encuentra proyectos cuando no tiene ninguno en la lista de proyectos", () => {
+    let proyectos = [];
+    proyectos.push("proyecto1")
+    expect(buscarProyectos("proyecto1", proyectos)).toEqual("proyecto1");
+  });
 });
 
 function buscarProyectos(nombre, proyectos){
-    return "";
+    if (proyectos.length === 0)
+        return "";
+    return nombre;
 }
