20512186fccc22659df6fd9dfb9adc1c28040c40
lPatrickll <patrickgonzalesquintana@gmail.com>
2024-05-07 10:23:19 -0400

    Se mejoro la funcion buscarProyectos para que busque un proyecto en una lista de proyectos

--- src/buscar.spec.js
+++ src/buscar.spec.js
@@ -4,15 +4,26 @@ describe("Buscar", () => {
     expect(buscarProyectos("proyecto1", proyectos)).toEqual("");
   });
 
-  it("no encuentra proyectos cuando no tiene ninguno en la lista de proyectos", () => {
+  it("encuentra un proyecto cuando el mismo existe en una lista de 1 proyecto", () => {
     let proyectos = [];
     proyectos.push("proyecto1")
     expect(buscarProyectos("proyecto1", proyectos)).toEqual("proyecto1");
+  });
+  
+  it("encuentra un proyecto cuando el mismo existe en una lista de varios proyectos", () => {
+    let proyectos = [];
+    proyectos.push("proyecto1");
+    proyectos.push("proyecto2");
+    proyectos.push("proyecto3");
+    expect(buscarProyectos("proyecto2", proyectos)).toEqual("proyecto2");
   });
 });
 
 function buscarProyectos(nombre, proyectos){
     if (proyectos.length === 0)
         return "";
-    return nombre;
+    for (let i = 0; i < proyectos.length; i++) {
+        if (proyectos[i] === nombre)
+            return proyectos[i];    
+    }
 }
